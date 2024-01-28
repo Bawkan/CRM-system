d98d7b0dfe7e22689e35d20299c8f6b95f6b06e8
Chingiz Nasibbeyli <reis_969@mail.ru>
2024-01-28 22:06:12 +0300

    Update tests.py

--- crmsystem/services/tests.py
+++ crmsystem/services/tests.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+from services.models import ServicesModel
 
-# Create your tests here.
+
+class ProductListViewTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.product = ServicesModel.objects.create(name="qwerty", price=225.21)
+        cls.users = User.objects.create_user(username="Bob", password="qwerty")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.product.delete()
+        cls.users.delete()
+
+
+    def test_product_list(self):
+        response = self.client.post(reverse("product:list"),
+                                    {
+                                        "name": "apple",
+                                        "description": "a good phone",
+                                        "price": "143",
+                                    }
+                                    )
+
+        self.assertTrue(response)
+
+    def test_product_list_page(self):
+        response = self.client.get(reverse("product:list"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_detail_page(self):
+        response = self.client.get(
+            reverse("product:detail", kwargs={"pk": self.product.pk})
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_authentication_user(self):
+
+        pathh = self.client.post(reverse("crm:login"), username=self.users.username, password=self.users.password)
+        self.assertEqual(pathh.status_code, 200)
+        self.assertTrue(pathh)
